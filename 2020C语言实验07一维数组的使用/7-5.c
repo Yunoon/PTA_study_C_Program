4447433a80434a38342020119757a6800a0d8bf7
Yunoon <yunoon233@gmail.com>
2020-11-25 19:32:03 +0800

    update

--- 2020C语言实验07一维数组的使用/7-5.c
+++ 2020C语言实验07一维数组的使用/7-5.c
@@ -5,7 +5,7 @@ int main()
     int a[10],n,m;
 
     scanf("%d",&n);
-    for (int i = n-1; i >= 0; i--)
+    for (int i = n-1; i >= 0; i--)//逆存放
     {
         /* code */
         scanf("%d",&a[i]);
