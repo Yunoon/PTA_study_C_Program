f1aacf2a82b291a19f9a8bce6dbf7f5a74f78dc8
Yunoon <yunoon233@gmail.com>
2020-12-09 20:04:22 +0800

    update

--- 2020C语言程序设计专题实验04/7-3.c
+++ 2020C语言程序设计专题实验04/7-3.c
@@ -9,19 +9,19 @@ int main()
     int sum[100]={0};
     int item_core[100];
     int item_res[100];
-    scanf("%d %d",&m,&n);
+    scanf("%d %d",&m,&n);//读取学生人数和判断题数量
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//每道题的满分值
     {
         /* code */
         scanf("%d",&item_core[i]);
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//每道题对应的正确答案
     {
         /* code */
         scanf("%d",&item_res[i]);
     }
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)//学生的解答
     {
         /* code */
         int sum_i=0;
