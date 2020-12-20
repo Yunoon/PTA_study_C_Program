9e0d807e11814c43b6b890368bf2d2a3e39b67a7
yunoon <yunoon233@gmail.com>
2020-12-21 01:45:52 +0800

    update

---
+++ PTA/PTA/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PTA
+//
+//  Created by Yunoon on 2020/12/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
