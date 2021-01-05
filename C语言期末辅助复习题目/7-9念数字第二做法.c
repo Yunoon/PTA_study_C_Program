de9800d3214620b66aadfc7fa6870c9696870d51
Yunoon <yunoon233@gmail.com>
2021-01-06 00:06:43 +0800

    字符型转整数型，yyds！！！

--- C语言期末辅助复习题目/7-9念数字第二做法.c
+++ C语言期末辅助复习题目/7-9念数字第二做法.c
@@ -7,15 +7,13 @@ int main()
 {   
     char n[50]={'\000'};
 
-    
     gets(n);
     if(n[0]==45)
     {
         printf("fu ");
         for (int i = 1; n[i]!=0; i++)
         {
-            /* code */
-            // printf("%c",n[i]); 
+            //这里使用了 int g = t - '0'; 把字符转为了整数型，特别巧妙！！！！
             char t = n[i];
             int g = t - '0';
             printf("%s ",biao[g]);
