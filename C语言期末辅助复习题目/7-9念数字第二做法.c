b7d6448b3c26880a3612811571e2cec5e92ddcf3
Yunoon <yunoon233@gmail.com>
2021-01-06 00:14:18 +0800

    字符转整形做法，yyds！！！

--- C语言期末辅助复习题目/7-9念数字第二做法.c
+++ C语言期末辅助复习题目/7-9念数字第二做法.c
@@ -5,22 +5,34 @@
 char *biao[]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
 int main()
 {   
-    char n[50]={'\000'};
+    char n[100000]={'\000'};
 
     gets(n);
     if(n[0]==45)
     {
-        printf("fu ");
+        printf("fu");
         for (int i = 1; n[i]!=0; i++)
         {
             //这里使用了 int g = t - '0'; 把字符转为了整数型，特别巧妙！！！！
             char t = n[i];
             int g = t - '0';
-            printf("%s ",biao[g]);
+            printf(" %s",biao[g]);
         }
         
     }
-    
-
+    else
+    {
+        /* code */
+        char t = n[0];
+        int g = t - '0';
+        printf("%s",biao[g]);
+        for (int i = 1; n[i]!=0; i++)
+        {
+            //这里使用了 int g = t - '0'; 把字符转为了整数型，特别巧妙！！！！
+            char t = n[i];
+            int g = t - '0';
+            printf(" %s",biao[g]);
+        }
+    }
     return 0;
 }
